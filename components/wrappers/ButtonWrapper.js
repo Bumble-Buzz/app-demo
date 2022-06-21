a57c48ecf5763859521a2d63464c1fa9d6881a57
arizvi125 <aaarizvi125@gmail.com>
2022-06-21 14:45:19 -0400

    Update, added socials. Added network info

--- components/wrappers/ButtonWrapper.js
+++ components/wrappers/ButtonWrapper.js
@@ -9,7 +9,7 @@ export default function ButtonWrapper({ children, ...props }) {
   return (
     <button
       type="button"
-      className={`py-2 px-2 inline-flex justify-center items-center content-center border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${classes}`}
+      className={`py-2 px-2 inline-flex justify-center items-center content-center border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 ${classes}`}
       {...props}
     >
       {children}
